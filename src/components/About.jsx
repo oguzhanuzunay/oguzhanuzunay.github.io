d5ffd4c94a8db031ea861ac84c445f96f36b17a6
Oguzhan <oguzhanzny@gmail.com>
2023-11-08 03:12:39 +0300

    constants are updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,69 +1,104 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/no-unknown-property */
-import React from 'react'
-import Tilt from 'react-parallax-tilt';
-import {motion} from 'framer-motion';
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import {fadeIn, textVariant} from '../utils/motion';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
-import {SectionWrapper} from '../hoc';
+import { SectionWrapper } from "../hoc";
 
-const ServiceCard = ({index,title,icon}) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className='xs:w-[250px] w-full'>
-      <motion.div 
-      variants={fadeIn('right', 'spring',0.5*index,0.75)} 
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        <div options={{
-            max:45,
-            scale:1,
-            speed:450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
-
-
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
-
       </motion.div>
     </Tilt>
-  )
-}
-
+  );
+};
 
 const About = () => {
   return (
     <>
-        <motion.div variants={textVariant()}>
-            <p className={styles.sectionSubText}> Introduction</p>
-            <h2 className={styles.sectionHeadText}>Overview</h2>
-        </motion.div>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-        <motion.p variants={fadeIn("","",0.1,1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'> 
-           <p className=' mb-5'>As an experienced Front-End Developer with over 3 years in the field, I have a proven track record working on global projects for major partners such as Samsung, Singapore Airlines, Alliance, and Hyundai across over 60 countries. I'm skilled in JavaScript, React, Node.js, CSS, Sass, and SQL, dedicated to developing high-quality software solutions that meet business needs.</p> 
-
-<h2 className='font-bold'>Key Skills:
-</h2> 
-<ul className=' list-disc ml-5 space-y-1'>
-  <li>
-  JavaScript (Object-Oriented Programming, Functional Programming, DOM Manipulation, Error Handling, etc.)
-  </li>
-  <li>React.js, Node.js, TypeScript
-</li>
-  <li>HTML5, CSS3, Sass</li>
-  <li>Git, Performance Optimization, Critical Render Path</li>
-</ul>
-        </motion.p>
-
-        <div className=' mt-10 flex flex-wrap gap-10 xs:justify-center'>
-          {services.map((service,index)=> (<ServiceCard key ={services.title} index={index} {...service}/>))}
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        <div className=" mb-5">
+          <p className="mb-5">
+            Passionate Front-End Developer with
+            <span className="font-bold">
+              &nbsp; 3+ years of professional working experience &nbsp;
+            </span>
+            with on building, fixing and optimize responsive, mobile first and
+            high conversion rate website in the top companies of Tech and
+            E-Commerce industry in fast-paced, collaborative environments.
+            <br /> I focus on creating better mobile-friendly responsive designs
+            with best practice and maximum performance by taking innovator and
+            creative problem-solving approach using BrowserStack and Lighthouse.
+            Love to work being collaborative team.
+          </p>
+          <h2 className="font-bold">TECHNICAL SKILLS:</h2>
+          <div className="mb-5 list-disk ml-5 space-y-1">
+            <li>Specialized in JavaScript, React.js, Next.js, Typescript</li>
+            <li>
+              Tailwind, Material UI, Bootstrap, Sass, Node.js, HTML, CSS,
+              JavaScript, Mongo, Git
+            </li>
+          </div>
+          <h2 className="font-bold">PROFESSIONAL HIGHLIGHTS:</h2>
+          <ul className="mb-5 list-none ml-5 space-y-3">
+            <li>
+              <span className="text-3xl">💯</span> 100% customer satisfaction
+              for 1-year period on Zendesk ticket management system (World-wide
+              customers as Singapur Airlines, Samsung, Allianz, CNN, Adidas,
+              Coco-Cola, and Toyota...).
+            </li>
+            <li>
+              <span className="text-3xl">📈 </span> Increased the completion
+              speed of each website opened for different regions of a global
+              brand by 70% with the innovative scripts and automations I have
+              designed.
+            </li>
+            <li>
+              <span className="text-3xl"> 📉 </span> Decreased Similar Website
+              development processes for teams by 60% for with the technical
+              documents, case studies and templates I have created.
+            </li>
+          </ul>
         </div>
-    </>
-  )
-}
+      </motion.p>
 
-export default SectionWrapper(About,"about")
+      <div className=" mt-10 flex flex-wrap gap-10 xs:justify-center">
+        {services.map((service, index) => (
+          <ServiceCard key={services.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,277 +1,276 @@
 import {
-    mobile,
-    backend,
-    creator,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
-    adcropper,
-    kartelam,
-    beko,
-    insider,
-    redqueen,
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "Web Developer",
-      icon: web,
-    },
-    {
-      title: "React Native Developer",
-      icon: mobile,
-    },
-    {
-      title: "Backend Developer",
-      icon: backend,
-    },
-    {
-      title: "Content Creator",
-      icon: creator,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-  ];
-  
-  const experiences = [
-    {
-      title: "Web Development Intern",
-      company_name: "Adcropper",
-      icon: adcropper,
-      iconBg: "#383E56",
-      date: "June 2019 - Sep 2019",
-      points: [
-        "Gained hands-on experience with Pure (Vanilla) JavaScript focusing on DOM manipulations, asynchronous programming, and Object-Oriented Programming.",
-        "Built tools/websites using HTML, CSS, and Vanilla JavaScript in real projects.",
-      ],
-    },
-    {
-      title: "Web Development Intern",
-      company_name: "Kartelam.com",
-      icon: kartelam,
-      iconBg: "#E6DEDD",
-      date: "Nov 2020 - Jan 2021",
-      points: [
-        "Gained hands-on experience as a Full Stack Developer in a test-driven environment, working on real projects using Git, and developing projects on React and backend services.",
-        " Acquired work experience with Go, React, HTML, CSS, Javascript, and GitLab in a test-driven environment.",
-      ],
-    },
-    {
-      title: "Software Consultant",
-      company_name: "Beko Global",
-      icon: beko,
-      iconBg: "#383E56",
-      date: "Jan 2021 - Sep 2021",
-      points: [
-        "Provided first-line technical support for over 60 countries.",
-        "Developed scripts to integrate countries' languages into the system, speeding up this process by 90%.",
-        "Debugged and developed high-quality Landing Pages (LPs) using JavaScript and CSS.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-      ],
-    },
-    {
-      title: "Technical Support Engineer",
-      company_name: "Insider",
-      icon: insider,
-      iconBg: "#E6DEDD",
-      date: "Sep 2021 - Feb 2023",
-      points: [
-        "Utilized Zendesk for ticket management and technical support, achieving 100% end-user satisfaction.",
-        "Identified, fixed, and improved errors in AI-driven products aiming to increase sales for e-commerce partners, while gaining experience in frontend technologies such as JavaScript and React.",
-        "Participated in 1:1 meetings with corporate partners to address their customization requests and reported issues, ensuring effective communication and problem-solving.",
-        "Managed technical integration processes and meetings with business partners, gaining valuable experience with front-end development tools and practices.",
-        "Prepared and shared a unified document (code standards, team responsibilities, and scope) for Global, MENA, and APAC Technical Support Engineering teams using Confluence."
-      ],
-    },
-    {
-      title: "Co-Founder",
-      company_name: "Red Queen Fashion",
-      icon: redqueen,
-      iconBg: "#E6DEDD",
-      date: "Feb 2023 - Present",
-      points: [
-        "Managed the construction, administration, setup, and development phases of the company's website.",
-        "Integrated previous professional experiences and business management processes into my own company.",
-        "Successfully implemented SEO, Conversion Rate Optimization, and email/sms automation strategies on our website.",
-        "Successfully implemented SEO, Conversion Rate Optimization, and email/sms automation strategies on our website.",
-        "Currently, our company is rapidly growing and preparing for international operations."
-      ],
-    },
-  ];
-  
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: jobit,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Trip Guide",
-      description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-      tags: [
-        {
-          name: "nextjs",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
-    },
-  ];
-  
-  export { services, technologies, experiences, testimonials, projects };
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  adcropper,
+  kartelam,
+  beko,
+  insider,
+  redqueen,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Front-End Web Developer",
+    icon: web,
+  },
+  {
+    title: "Next.js  Full Stack Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Development Intern",
+    company_name: "Adcropper",
+    icon: adcropper,
+    iconBg: "#383E56",
+    date: "June 2019 - Sep 2019",
+    points: [
+      "Hands-on experience with Vanilla JavaScript focusing on DOM manipulations, asynchronous programming, and Object-Oriented Programming.",
+      "Builded e-mail template using HTML, CSS, and Vanilla JavaScript.",
+    ],
+  },
+  {
+    title: "Front-End Developer",
+    company_name: "Kartelam.com",
+    icon: kartelam,
+    iconBg: "#E6DEDD",
+    date: "Nov 2020 - Jan 2021",
+    points: [
+      "Gained hands-on experience as a Full Stack Developer in a test-driven environment, working on real projects using Git, and developing projects on React and backend services.",
+      " Acquired work experience with Go, React, HTML, CSS, Javascript, and GitLab in a test-driven environment.",
+    ],
+  },
+  {
+    title: "Software Consultant",
+    company_name: "Beko Global",
+    icon: beko,
+    iconBg: "#383E56",
+    date: "Jan 2021 - Sep 2021",
+    points: [
+      "Provided first-line technical support for over 60 countries.",
+      "Developed scripts to and reduce 90%-time cost for process of integrate countries languages into the system.",
+      "Debugged and developed high-quality Landing Pages (LPs) using HTML, CSS, Javascript and React.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+  {
+    title: "Front End Developer",
+    company_name: "Insider",
+    icon: insider,
+    iconBg: "#E6DEDD",
+    date: "Sep 2021 - Feb 2023",
+    points: [
+      "Served front end support to major corporate partners like Singapore Airlines, Samsung, Allianz, MAC, CNN, Adidas, Coco-Cola, and Toyota resolving unknown technical issues,  via 1:1 meetings and tickets by achieving 100% end-user satisfaction.",
+      "Identified, fixed errors and handled customization requests %100 optimized performance in AI-driven products aiming to increase sales and maximize conversion rate for e-commerce partners.",
+      "Managed technical integration processes (Shopify, Adobe Magento etc. and Hard code) via meetings with business partners, ensuring effective communication, problem-solving and maximum customer satisfaction.",
+      "Released technical documents (code standards, team responsibilities, and scope etc.) for Global, MENA, and APAC Technical teams via Confluence.",
+      "Developed and maintained the enterprise partners websites using HTML, CSS, JavaScript, and React.",
+    ],
+  },
+  {
+    title: "Co-Founder",
+    company_name: "Red Queen Fashion",
+    icon: redqueen,
+    iconBg: "#E6DEDD",
+    date: "Feb 2023 - Present",
+    points: [
+      "Managed the construction, administration, setup, test, and development phases of the company's website.",
+      "Implemented SEO, Conversion Rate Optimization, and email/sms automation strategies on our website.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Oguzhan does.",
+    name: "İlker Ayaz",
+    designation: "Head of Operational Excellence",
+    company: "INSIDER",
+    image:
+      "https://media.licdn.com/dms/image/C4E03AQGEd4inI2WAYA/profile-displayphoto-shrink_100_100/0/1574844247412?e=1704931200&v=beta&t=42hULlP0NnX9ztVf9y74B_-W7LEoNTyITXHitOvm8Kk",
+  },
+  {
+    testimonial:
+      "Oguzhan's creative and innovative solutions accelerated our development process considerably. He has a truly solution-oriented mind.",
+    name: "Tuğba Saklakoğlu",
+    designation: "Senior IT Lead and Senior Business Analyst",
+    company: "ARÇELİK GLOBAL",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQGIRKzyVTTuhQ/profile-displayphoto-shrink_200_200/0/1665849402026?e=1704931200&v=beta&t=HkcCxafjhbXJDHuZpd--WHEhNaqBCheSM-WbW9Xayhs",
+  },
+  {
+    testimonial:
+      "After Oguzhan optimized our website, our traffic increased by 45% and conversion rate 15%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
