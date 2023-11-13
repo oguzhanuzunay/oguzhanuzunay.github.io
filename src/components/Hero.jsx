d5da0577c350d773b40b8f0b15a6d097f2e189c0
Oguzhan <oguzhanzny@gmail.com>
2023-11-13 05:40:55 +0300

    add: Github logo and link to name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,52 +1,64 @@
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
-import {styles} from '../styles';
-import {ComputersCanvas} from './canvas';
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import { github } from "../assets";
 
 const Hero = () => {
-    return (
-        <section className='relative w-full h-screen mx-auto'>
-            <div
-                className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-                <div className=" flex flex-col justify-center items-center mt-5">
-                    <div className='w-5 h-5  rounded-full  bg-[#915eff]'/>
-                    <div className='w-1 sm:h-80 h-40  violet-gradient'/>
-                </div>
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10`}
+      >
+        <div className=" flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5  rounded-full  bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40  violet-gradient" />
+        </div>
 
-                <div>
-                    <h1 className={`${styles.heroHeadText}`}>Hi, I&apos;m
-                        <span className='text-[#915eff]'>&nbsp;Oğuzhan</span>
-                    </h1>
-                    <p className={`${styles.heroSubText} mt-2 text-white`}>
-                        I develop 3D visual, user
-                        <br className='sm:block hidden'/>
-                        interfaces and web applications
-                    </p>
-                </div>
-            </div>
+        <div className="w-full">
+          <h1 className={`${styles.heroHeadText} `}>
+            Hi, I&apos;m
+            <a
+              href="https://github.com/oguzhanuzunay"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="text-[#915eff]">
+                &nbsp;
+                <img src={github} height={80} width={80} className="inline " />
+                ğuzhan
+              </span>
+            </a>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white`}>
+            I develop 3D visual, user
+            <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
 
-            <ComputersCanvas/>
+      <ComputersCanvas />
 
-            <div
-                className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-                <a href='#about'>
-                    <div
-                        className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-starts p-2'>
-                        <motion.dev
-                            animate={{
-                            y: [0, 24, 0]
-                        }}
-                            transition={{
-                            duration: 1.5,
-                            repeat: Infinity,
-                            repeatType: 'loop'
-                        }}
-                            className="w-3 h-3 rounded-full bg-secondary mb-1"></motion.dev>
-                    </div>
-                </a>
-            </div>
-        </section>
-    )
-}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-starts p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            ></motion.dev>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
